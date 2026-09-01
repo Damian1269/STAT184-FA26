c72fa079d934d5809871d38be93138730521c287
Damian1269 <carloscalle107@yahoo.com>
2026-09-01 15:32:19 -0400

    Add files via upload

---
+++ Demo.R
@@ -0,0 +1,36 @@
+1+1
+R.version.string
+
+
+
+
+x <-10 # x is assigned as 10
+
+
+
+numbers <- 1:5
+average <- mean(numbers)
+
+rm(average)
+
+rm(list = ls())
+
+
+plot(1:10)
+list.files()
+
+
+?mean
+
+
+
+getwd()
+
+search()
+head(installed.packages()[, c("Package", "Version")])
+packageVersion("ggplot2")
+.libPaths()
+help(package = "ggplot2")
+vignette(package = "ggplot2")
+update.packages()
+
